bac6e1219e719a5b34578c787f413282edc98fdc
Md. Jobayer Hossain <jobayerhossain298@gmail.com>
2024-01-15 23:38:23 +0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import ingredients from '../../ingredients.json'
+
+export default function Home() {
+  return (
+    <main className="container mx-auto py-5">
+      <h1 className="text-4xl font-bold text-center">Recipe app</h1>
+    </main>
+  )
+}
