e791b198c2a3ea639c47190a8f3ad97b13aaa5aa
Md. Jobayer Hossain <jobayerhossain298@gmail.com>
2024-01-19 22:32:18 +0600

    changed

--- src/app/recipe/[id]/page.jsx
+++ src/app/recipe/[id]/page.jsx
@@ -1,36 +1,41 @@
+"use client";
+import { useEffect, useState } from "react";
 import Title from "../../component/title/Title";
 
-const getData = async (params) => {
-  const response = await fetch(
-    `https://recipe-server-wine.vercel.app/recipe/${params.id}`
-  );
-  return response.json();
-};
-
-const SingleRecipe = async ({ params }) => {
-  const data = await getData(params);
-  console.log(data);
+const SingleRecipe = ({ params }) => {
+  const [myData, setData] = useState({});
+  useEffect(() => {
+    fetch(`https://recipe-server-wine.vercel.app/recipe/${params.id}`)
+      .then((res) => res.json())
+      .then((result) => setData(result));
+  }, []);
+  console.log(myData);
   return (
     <div className="container mx-auto">
       <Title title="Recipe Details" />
       <div className="w-1/2 mx-auto shadow-md rounded-md p-10 my-10 font-bold capitalize">
         <h1 className="text-[17px] text-gray-600 mb-3">
-          <span className="text-md text-primary">Name:</span> {data.title}
+          <span className="text-md text-primary">Name:</span> {myData.title}
         </h1>
         <p className="text-[17px] text-gray-600 mb-3">
-          <span className="text-md text-primary">price:</span> {data.price} tk
+          <span className="text-md text-primary">price:</span> {myData.price} tk
         </p>
         <ul className="text-[17px] text-gray-600 mb-3">
           <span className="text-md text-primary">Ingredients:</span>
-          {data.ingredients.map((item, index) => (
-            <li key={item}>
-              {(index += 1)}. {item}
-            </li>
-          ))}
+          {myData && myData.ingredients && (
+            <ul className="text-[17px] text-gray-600 mb-3">
+              <span className="text-md text-primary">Ingredients:</span>
+              {myData.ingredients.map((item, index) => (
+                <li key={item}>
+                  {index + 1}. {item}
+                </li>
+              ))}
+            </ul>
+          )}
         </ul>
         <p className="text-[17px] text-gray-600 mb-3">
           <span className="text-md text-primary">instruction:</span>{" "}
-          {data.instruction}
+          {myData.instruction}
         </p>
       </div>
     </div>
